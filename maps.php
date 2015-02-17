9bf1ca6291a3f8e7e1da412eecb472ce205714bc
BestThemeRoad <BestThemeRoad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 05:40:06 +0000

    upload
    git-svn-id: https://plugins.svn.wordpress.org/easy-responsive-google-map/trunk@1092116 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- maps.php
+++ maps.php
@@ -2,7 +2,7 @@
 
 
 /**
- * Plugin Name: Easy_Responsive_Google_Map
+ * Plugin Name: Easy Responsive Google Map
  * Plugin URI: http://nakshighor.com
  * Description:  Are you want to add a Responsive Google Maps in your website? No more work hard to add maps in your sites. This plugin give you the best oppurtunity to add your sites easily. Just install this plugin as like other wordpress plugin.Then where you want to add the map for example pages or posts you just click Google map button. Then you clink simple Google Map button on the top.Then going to https://www.google.com.bd/maps/@23.7806365,90.4193257,12z this link and select your place and then after clinking settins and copy the embed link and paste google map plugin embed option.Then fill up the option custom width and height as you wish.Now publish the page or posts and enjoy the Easy Responsive Google Map Plugin. Don't Forget to give us good rating.
  * Version: 1.0.0
